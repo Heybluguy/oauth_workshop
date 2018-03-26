a11bf62d3f4e9eec02b474cd19a92fa822279e96
heybluguy <heybluguy@yahoo.com>
2018-03-25 23:53:37 -0400

    Token.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,16 @@ class SessionsController < ApplicationController
     client_secret = YOUR_CLIENT_SECRET
     code          = params[:code]
     response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+
+    pairs = response.body.split("&")
+    response_hash = {}
+    pairs.each do |pair|
+      key, value = pair.split("=")
+      response_hash[key] = value
+    end
+
+    token = response_hash["token"]
+
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
   end
 end
