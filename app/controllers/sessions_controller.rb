e2e96eeebd42ed3ba91f679a4a83f3d64f10622a
heybluguy <heybluguy@yahoo.com>
2018-03-25 23:44:57 -0400

    Add create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
+    client_id     = YOUR_CLIENT_ID
+    client_secret = YOUR_CLIENT_SECRET
+    code          = params[:code]
+    response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
   end
 end
